6cbc26a5baa7194ebfe410b887a4f59a111a009f
Alexander <eskander.aleks@gmail.com>
2025-08-24 09:17:42 +0300

    add function

--- navigator/colculate-distance.js
+++ navigator/colculate-distance.js
@@ -5,16 +5,19 @@
     // Точка назначения (например, Санкт-Петербург)
     const positionLat = 59.9343;
     const positionLong = 30.3351;
-    
-    // Расчет расстояния
+    function calculateDistance(addressLat, addressLong, positionLat, positionLong) {
+        // Расчет расстояния
     const DEGREE_TO_KM = 111.32;
     
     // Вычисляем разницу в координатах
-    const deltaLat = (positionLat - addressLat) * DEGREE_TO_KM;;
-    const deltaLon = (positionLong - addressLong) * DEGREE_TO_KM * Math.cos((addressLat + positionLat) * Math.PI / 360);
+        const deltaLat = (positionLat - addressLat) * DEGREE_TO_KM;;
+        const deltaLon = (positionLong - addressLong) * DEGREE_TO_KM * Math.cos((addressLat + positionLat) * Math.PI / 360);
     
     // Вычисляем расстояние по теореме Пифагора (диагональ прямоугольника)
-    const distance = Math.sqrt(deltaLat * deltaLat + deltaLon * deltaLon);
+        return Math.sqrt(deltaLat * deltaLat + deltaLon * deltaLon);
+    }
+    
+    const distance = calculateDistance(addressLat, addressLong, positionLat, positionLong);
     
     // Вывод результата
     console.log(`Расстояние до точки назначения: ${distance.toFixed(2)} км`);
