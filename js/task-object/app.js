15b8bf9ffcd04cbbee577cd8918a3c79f3c7d828
Alexander <eskander.aleks@gmail.com>
2025-09-08 08:21:57 +0300

    task app

---
+++ js/task-object/app.js
@@ -0,0 +1,23 @@
+const toDoLists  {
+    tasks: [{
+        id: 324234,
+        title: "Купить хлеб",
+        priority: 1,
+    }],
+    findTask: function(id) {
+        const task = this.tasks.find(task => task.id === id);
+        if (!task) {
+            console.log("Задача не найдена");
+            return null;
+        }
+        return task;
+    },
+    addTask: function(task) {
+        const id = new Date().getTime();
+        this.tasks.push({{...task, id}});
+    },
+    deleteTask: function(id) {
+        this.tasks = this.tasks.filter(task => task.id !== id);
+    },
+
+}
