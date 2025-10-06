471ae0545df255f729adf38f9778ef102aa3a1fa
Alexander <eskander.aleks@gmail.com>
2025-10-06 08:11:43 +0300

    Добавил проверку и принимаемые аргументы

--- js/birthday/app.js
+++ js/birthday/app.js
@@ -1,6 +1,5 @@
 function checkBirthday(age) {
     const nowYear = new Date().getFullYear();
-    console.log(nowYear);
-    return  nowYear - Date(age).getFullYear() > 14;
-    
+
+    return  nowYear - Date(age).getFullYear() >= 14;
 }
--- js/d6/app.js
+++ js/d6/app.js
@@ -1,3 +1,7 @@
-function dice() {
-    return Math.floor(Math.random() * 6) + 1;
+function dice(dice) {
+    if(dice <= 20 && dice >= 2 && dice % 2 === 0) {
+        return Math.floor(Math.random() * dice) + 1;
+    }
+
+    return 0;
 }
