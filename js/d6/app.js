5dc83e17fb2170da5f79c7fecd7d1c136b9e505d
Alexander <eskander.aleks@gmail.com>
2025-10-07 07:29:32 +0300

    Поправил замечания checkargs

--- js/d6/app.js
+++ js/d6/app.js
@@ -1,6 +1,7 @@
 function checkargs(str) {
-    if (str.substr(0) === "d") {
-        const dice = Number(str.substr(1));
+    const dice = Number(str.substr(1))
+    if (str.substr(0) === "d" && typeof dice === "number") {
+        ;
         return dice <= 20 && 
         dice >= 2 && 
         dice % 2 === 0;
